cc9d1e80d8f380a875eb259e5551dcf0f97266ae
MominIshtiaq <momin989ishtiaq@gmail.com>
2025-12-08 14:11:59 +0500

    add JwtModule to and authModule imports and async registered authConfig

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,9 +6,27 @@ import { HashingProvider } from './provider/hashing.provider';
 import { BcryptProvider } from './provider/bcrypt.provider';
 import { ConfigModule } from '@nestjs/config';
 import authConfig from './config/auth.config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [forwardRef(() => UserModule), ConfigModule.forFeature(authConfig)],
+  imports: [
+    forwardRef(() => UserModule),
+    ConfigModule.forFeature(authConfig),
+    /* 
+    asProvider method will help us by avoiding extra boilerplate
+    if we don't use this method what we have to do is
+    JwtModule.registerAsync({
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: (configService: ConfigService) => ({
+            JWT_TOKEN_SECRET_KEY : configService.get('authConfig.secret'),
+            JWT_TOKEN_EXPIRESIN: configService.get('authConfig.expiresIn'),
+            // so on
+          }),
+        })
+    */
+    JwtModule.registerAsync(authConfig.asProvider()),
+  ],
   controllers: [AuthController],
   providers: [
     AuthService,
